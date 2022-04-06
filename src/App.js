183e492ac6697ee8c7cdfecff1f2334608b6e39e
souadhimel <souad.anam@gmail.com>
2022-04-06 17:18:02 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
 import './App.css';
 import { FcGoogle} from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 
 const auth = getAuth(app);
 
 
 function App() {
-  const googleProvider= new GoogleAuthProvider();
 
+  // Provider
+  const googleProvider= new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+
+
+// storing data
   const [user,setUser]=useState({})
 
 
@@ -27,23 +33,43 @@ function App() {
     })
   }
 
+// Github sign in
 
-  // Sign out
-const handleGoogleSignOut=()=>{
-  signOut(auth)
-  .then(() => {
-    setUser({})
-  }).catch((error) => {
-    setUser({})
-  });
-  
+const handleGithubLogin=()=>{
+  signInWithPopup(auth,gitHubProvider)
+    .then(result=>{
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+
 }
+
+  // Sign out (google/github)
+  const handleGoogleSignOut=()=>{
+    signOut(auth)
+    .then(() => {
+      setUser({})
+    }).catch((error) => {
+      setUser({})
+    });
+    
+  }
 
   return (
     <div className="App">
      <h2>Firebase Authentication</h2>
-     <button onClick={handleGoogleLogin}>Google authentication <FcGoogle></FcGoogle> </button>
-     <button onClick={handleGoogleSignOut}>Sign out</button>
+   {
+user.uid?<button onClick={handleGoogleSignOut}>Sign out  <FcGoogle></FcGoogle></button>: 
+<>
+<button onClick={handleGoogleLogin}>Google authentication  <FcGoogle></FcGoogle> </button>
+<button onClick={handleGithubLogin}>Github Login  <BsGithub></BsGithub></button>
+</> 
+   }
+   
      <h2>{user.displayName}</h2>
      <h3>{user.email}</h3>
      <img src={user.photoURL} alt="" />
