b209cadf57b70af05403b395da14f9debbc42aee
souadhimel <souad.anam@gmail.com>
2022-04-06 15:07:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { FcGoogle} from "react-icons/fc";
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+
+
+const auth = getAuth(app);
 function App() {
+  const googleProvider= new GoogleAuthProvider();
+  const handleGoogleLogin=()=>{
+    signInWithPopup(auth,googleProvider)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Firebase Authentication</h2>
+     <button onClick={handleGoogleLogin}>Google authentication <FcGoogle></FcGoogle> </button>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_yY-lI-saNsBtTd42QvWlKiplAXOOtiw",
+  authDomain: "auth-practice-6facf.firebaseapp.com",
+  projectId: "auth-practice-6facf",
+  storageBucket: "auth-practice-6facf.appspot.com",
+  messagingSenderId: "456082385699",
+  appId: "1:456082385699:web:b9f75ecef19fba2613fa96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
